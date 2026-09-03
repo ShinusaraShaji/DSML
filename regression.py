3ecdc0ac4af4e4328cdb092f3755608252ceb3de
Shinu Shaji <shinushaji306@gmail.com>
2026-09-03 10:11:49 +0530

    commit

---
+++ regression.py
@@ -0,0 +1,8 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+X=np.array([[1],[2],[3],[4],[5]])
+y=np.array([2,4,5,4,5])
+model = LinearRegression().fit(X,y)
+print("Coefficient:",model.coef_[0])
+print("Intercept:",model.intercept_)
+print("Prediction for x=6:",model.predict([[6]])[0])
