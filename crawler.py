462f50707081c35caba52c8e7e69d3a7d2316d91
Shinu Shaji <shinushaji306@gmail.com>
2026-09-10 11:32:24 +0530

    commit

--- crawler.py
+++ crawler.py
@@ -1,15 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
-URL="https://realpython.github.i0/fake-jobs/"
-page=requests.get(URL)
-print(page.text)
-soup=BeautifulSoup(page.content,"html.parser")
-results=soup.find(id="ResultsContainer")
-job_elements=results.find_all("div",class_="card-copntent")
-for job_elents in job_elements:
-    title_element=job_elements.find("h2",class_="title")
-    company_element=job_elements.find("h3",class_="company")
-    location_element=job_elements.find("p",class_="location")
+
+URL = "https://realpython.github.io/fake-jobs/"
+
+page = requests.get(URL)
+
+print(page.status_code)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id="ResultsContainer")
+
+job_elements = results.find_all("div", class_="card-content")
+
+for job_element in job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+
     print(title_element.text.strip())
     print(company_element.text.strip())
+    print(location_element.text.strip())
     print()
